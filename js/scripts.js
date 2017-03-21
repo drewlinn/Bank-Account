fd13780e48f09001b88cdb74f1dee32aa0ff6b66
Andrew Dalton and Emily Dokken <andrew@>
2017-03-20 20:56:35 -0700

    add prototypes for depost and withdrawal in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
 //business logic
+function Account(name, balance) {
+  this.userName = name;
+  this.balance = balance;
+};
 
-
+Account.prototype.deposit = function() {
+    newAccount.balance + depositAmount
+};
+Account.prototype.withdrawal = function()  {
+    newAccount.balance - withdrawalAmount
+};
 
 //user interface logic
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#register").submit(function(event) {
     event.preventDefault();
+    var name = $("input#name").val();
+    var initialDeposit = parseInt($("input#initial").val());
+    var newAccount = new Account(name, initialDeposit);
+    console.log(newAccount);
+  });
+
+  $("form#deposit").submit(function(event) {
+    event.preventDefault();
+    var depositAmount = parseInt($("input#depositAmount").val());
+    var withdrawalAmount = parseInt($("input#withdrawalAmount").val());
+
+    // need to create an if statement that calls the specific prototype based on whether deposit or withdrawal is input within the form.
 
   });
 });
